d78bba97c67a3b36a4ba4c1dcc6f094b3050420e
gogobebe2 <gogobebe2@gmail.com>
2017-01-27 01:01:28 +1300

    Initial check-in of module GoogleKeywordPopularityFinder

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Web;
+using System.Web.UI;
+
+namespace GoogleKeywordPopularityFinder
+{
+	class MainClass
+	{
+		public static void Main (string [] args)
+		{
+			Console.WriteLine ("Hello World!");
+			runScript ("<script type='text/javascript'>functionname1();</script>", "Test");
+		}
+
+		public static void runScript (string javaScript, string key)
+		{
+			Page page = HttpContext.Current.CurrentHandler as Page;
+			page.ClientScript.RegisterStartupScript (
+				typeof (Page),
+				key,
+			javaScript);
+		}
+	}
+}
